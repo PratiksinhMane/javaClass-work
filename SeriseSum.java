67b111586c4da054cd32a55e9d5ae070bab726e8
Pratiksinh Mane <68189414+PratiksinhMane@users.noreply.github.com>
2020-08-01 21:40:00 +0530

    seriseSum

---
+++ SeriseSum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SeriseSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+				int n = sc.nextInt();
+				float result =0;
+				
+//				for (float i= 1; i<=n; i++) {
+//					result += 1/i;
+//					
+//				}
+//				System.out.println(result);
+	for (float i = 1; i<=n; i++) {
+		if (i % 2 == 0) {
+		result -= 1/i;
+	}else
+		result += 1/i;
+	}
+
+}
+	System.out.println(result);
+}
