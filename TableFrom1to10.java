46adb48c1f5e372d6c7e056a407fec60f67c177e
Pratiksinh Mane <68189414+PratiksinhMane@users.noreply.github.com>
2020-08-01 21:40:12 +0530

    tables

---
+++ TableFrom1to10.java
@@ -0,0 +1,15 @@
+
+public class TableFrom1to10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int j =1; j<=20;j++) {
+			int tableOf = j;
+			for (int i=1; i<=10; i++) {
+				System.out.print(tableOf * i +"  ");
+			}
+			System.out.println(  );
+		}
+	}
+
+}
